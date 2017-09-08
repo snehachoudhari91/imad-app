1b5bb46e2e7ef3fe9cbd53b3c6ad602a3ba1926b
snehachoudhari91 <31094127+snehachoudhari91@users.noreply.github.com>
2017-09-08 02:14:05 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 var e=document.getElementById("img");
+function moveLeft()
+{
+    var marginLeft=0;
+    marginLeft=marginLeft-10;
+    e.style.marginLeft=marginLeft+"10px";
+};
 e.onclick=function()
 {
     var interval=setInterval(moveLeft,100);
