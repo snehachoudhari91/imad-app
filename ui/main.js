e448fb48186b78dabdf2c362204ee824d59868f2
snehachoudhari91 <31094127+snehachoudhari91@users.noreply.github.com>
2017-09-08 02:14:41 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ function moveLeft()
 {
     var marginLeft=0;
     marginLeft=marginLeft-10;
-    e.style.marginLeft=marginLeft+"10px";
+    e.style.marginLeft=marginLeft+"1px";
 };
 e.onclick=function()
 {
-    var interval=setInterval(moveLeft,100);
+    var interval=setInterval(moveLeft,50);
 
 };
