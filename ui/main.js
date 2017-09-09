96de05c66369ff342eaf22c1d7f7733f6de3401a
snehachoudhari91 <31094127+snehachoudhari91@users.noreply.github.com>
2017-09-08 21:47:38 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick=function()
     };
     
 //make the request
-request.open('GET',"http://snehamcse.imad.hasura-app.io/counter",true);
+request.open('GET','http://snehamcse.imad.hasura-app.io/counter',true);
 request.send(null)
 
    
